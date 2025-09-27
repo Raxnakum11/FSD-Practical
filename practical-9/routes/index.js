e571ce07c1ff4f954b1eebcd0a44984c63141558
Raxnakum11 <148608416+Raxnakum11@users.noreply.github.com>
2025-09-27 13:55:53 +0530

    Remove all files from repository

--- practical-9/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-// Home route: Respond with a welcome message
-router.get("/", (req, res) => {
-  res.send("Welcome to our site");
-});
-
-module.exports = router;
